5710553f97f43bf06205a54c4d21e8c384d66110
Jamerzbatman <jamiethetec@gmail.com>
2024-10-19 08:56:05 -0700

    sjgfs

--- quotes/config/settings.py
+++ quotes/config/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quotes',
+    'movies',
+    'accounts',
+    'youtube',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
